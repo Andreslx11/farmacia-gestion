d19b7e2dfcfd90501ecdee7ce418d1f4b59bb351
Andres Muñoz Taborda <andreslx11@gmail.com>
2024-10-04 15:26:28 -0500

    feat(Proveedor): Creación de la clase Proveedor.

---
+++ src/main/java/com/example/gestionfarmacia/persistence/entity/Proveedor.java
@@ -0,0 +1,42 @@
+package com.example.gestionfarmacia.persistence.entity;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Lombok
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+// Jpa
+@Entity
+@Table(name = "proveedores")
+public class Proveedor {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre_empresa" )
+    private String nombreEmpresa;
+
+    private String contacto;
+    private String telefono;
+    private String email;
+    private String nit;
+
+
+    @OneToMany(mappedBy = "proveedor")
+    private List<Producto> productos = new ArrayList<>();// es necesario para que es
+
+
+}

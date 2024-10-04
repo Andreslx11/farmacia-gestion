8ca29080e6ab50e5fded667d72471fa48ea43401
Andres Muñoz Taborda <andreslx11@gmail.com>
2024-10-04 15:25:44 -0500

    feat(Producto): Creación de la clase Producto.

---
+++ src/main/java/com/example/gestionfarmacia/persistence/entity/Producto.java
@@ -0,0 +1,65 @@
+package com.example.gestionfarmacia.persistence.entity;
+
+import com.example.gestionfarmacia.persistence.enums.productoenums.Categoria;
+import com.example.gestionfarmacia.persistence.enums.productoenums.Concentracion;
+import com.example.gestionfarmacia.persistence.enums.productoenums.Presentacion;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+// Lombok
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+
+// Jpa
+@Entity
+@Table(name = "productos" )
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    @Column( name = "id_producto")
+    private long id;
+
+
+    @Column( name = "nombre_comercial")
+    private String  nombreComercial;
+
+    @Column( name = "nombre_generico" )
+    private String nombreGenerico;
+
+    private  String laboratorio;
+
+    @Enumerated(EnumType.STRING)
+    private Presentacion presentacion;
+
+    @Enumerated(EnumType.STRING)
+    private Concentracion concentracion;
+
+    private Integer stock;
+
+    @Column( name = "precio_venta")
+    private BigDecimal precioVenta;
+
+    @Column( name = "fecha_vencimiento" )
+    private LocalDate fechaVencimiento;
+
+    @Enumerated(EnumType.STRING)
+    private Categoria categoria;
+
+    @Column(name = "registro_invima")
+    private String registroInvima;
+
+    @ManyToOne
+    @JoinColumn(name = "proveedor_id", referencedColumnName = "id_proveedor")
+    private Proveedor proveedor;
+
+}

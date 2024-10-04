ea1946dd4bf6f02605f74acea12f29af718d470d
Andres Muñoz Taborda <andreslx11@gmail.com>
2024-10-04 15:12:16 -0500

    Inicialización del proyecto

---
+++ src/main/java/com/example/gestionfarmacia/GestionfarmaciaApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestionfarmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionfarmaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionfarmaciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gestionfarmacia/GestionfarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestionfarmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionfarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
